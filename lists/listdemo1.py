12a03acc14bf3c8b6e07dbfebba227552a64e283
Solomon Antony <you@example.com>
2024-09-11 07:23:19 -0500

    added first def to listdemo1

--- lists/listdemo1.py
+++ lists/listdemo1.py
@@ -1,2 +1,8 @@
 # write an app that interacts with the user to add items to a list, remove an item 
 # from the list, and print the list
+def add_item(list_name, item):
+    if not list_name:
+        list_name = []
+    list_name.apppend(item)
+    return(list_name)
+        
